21eefca3dd3e980bb7a2a8d9ee6112522b93d00e
WatchMeGuys <79733103+WatchMeGuys@users.noreply.github.com>
2021-07-24 02:56:44 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from cx_Freeze import setup, Executable
+
+executables = [Executable('main.py', target_name='construct.exe', base='Win32GUI',icon='construct.ico')]
+include_files = ['LimboQT.ui', 'supreme_access.ui', 'supreme_window.ui', 'username_change_window.ui']
+options = {
+    'build_exe': {
+        'include_msvcr': True,
+        'build_exe': 'build_windows',
+        'include_files': include_files,
+    }
+}
+
+setup(name='hello_world',
+      version='0.0.2',
+      description='construct.exe',
+      executables=executables,
+      options=options)
