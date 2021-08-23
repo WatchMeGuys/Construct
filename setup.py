000d25505146a864e0854919c8f0d1ab10b0d4c9
WatchMeGuys <79733103+WatchMeGuys@users.noreply.github.com>
2021-08-23 20:23:35 +0300

    Updated

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from cx_Freeze import setup, Executable
 
 executables = [Executable('main.py', target_name='construct.exe', base='Win32GUI', icon='construct.ico')]
 
-include_files = ['/UIFiles/Construct.ui', '/UIFiles/supreme_access.ui', '/UIFiles/supreme_window.ui', '/UIFiles/username_change_window.ui',
-                 '/UIFiles/update.ui', '/UIFiles/profile_images/', '/UIFiles/TextFiles/']
+include_files = ['UIFiles', 'profile_images', 'TextFiles']
 options = {
     'build_exe': {
         'include_msvcr': True,
